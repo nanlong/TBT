d35a0d6629d11a42f460c59a7722c8f264157373
lixp <894646171@qq.com>
2019-05-12 22:52:13 +0800

    beta1

---
+++ CoinPool.sol
@@ -0,0 +1,116 @@
+pragma solidity >=0.4.0;
+
+contract TRC20 {
+    function transfer(address to, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+}
+
+contract CoinPool{
+    // owner 合约拥有者, 拥有合约所有的权限. owner为合约创建者, 暂不支持更改.
+    // 一定要保存好owner的私钥, 做好备份并禁止泄露.
+    address public  owner;
+    // 资金池总开关. true:表示打开, false:表示锁定.
+    // 锁定状态下所有Game都无法动用资金. owner提现不受限制.
+    bool public openning;
+    // key:游戏合约地址
+    // value:是否允许动用资金 true:允许 false:禁止
+    mapping(address=>bool) public games;
+    // 杠杆比率, 即游戏合约转账必须满足 transferAmount * leverRadio <= poolBalance
+    // 比如资金池资金(poolBalance)=100, 杠杆比率(leverRadio)=10, 则游戏合约每次最多只能转账(transferAmount) = 10
+    // 如果超过这个限度, 则转账失败, 不会发生任何的资金变动.
+    uint256 public leverRadio;
+
+    TRC20 public tbt;
+
+    // onlyOnwer 表示只有owner才可以调用此方法
+    modifier onlyOwner(){require(msg.sender==owner);_;}
+    // mustOpen 表示只有资金池处于打开状态, 并且对应的game允许动用资金, 才可以通过
+    modifier onlyGamer(){require(openning==true&&games[msg.sender]==true);_;}
+
+    // 创建合约,设置owner为创建者
+    // 权限: 无
+    // 参数: _openning 资金池状态
+    //      _leverRadio 资金比率
+    constructor(bool _openning, uint256 _leverRadio, TRC20 _tbt) public{
+        owner = msg.sender;
+        openning = _openning;
+        leverRadio = _leverRadio;
+        tbt = _tbt;
+    }
+    // 增加游戏合约地址,增加后该合约即可以使用资金(在资金池解锁的状态下)
+    // 权限: owner
+    // 参数: _game 游戏合约地址
+    function addGame(address _game) external onlyOwner{
+        games[_game] = true;
+    }
+    // 停止某个游戏合约, 停止后该合约即无法使用资金
+    // 权限: owner
+    // 参数: _game 停止的游戏合约地址
+    function stopGame(address _game) external onlyOwner{
+        games[_game] = false;
+    }
+
+    // 冻结资金池, 所有的游戏均无法使用资金
+    // 权限: owner
+    function lock() external onlyOwner{
+        openning = false;
+    }
+    // 解锁资金池
+    // 权限: owner
+    function unlock() external onlyOwner{
+        openning = true;
+    }
+
+    // 修改资金比率
+    // 权限: owner
+    // 参数: _leverRadio 新比率
+    function changeLeverRadio(uint256 _leverRadio) external onlyOwner{
+        leverRadio = _leverRadio;
+    }
+
+    // 提现到owner
+    // 权限: owner
+    // 参数: _amount 提现金额, 如果提现金额比资金池大, 则提走全部资金 
+    function withdraw(uint256  _amount) external onlyOwner{
+        owner.transfer(_amount);
+    }
+
+    function withdrawToken(uint256 _amount, uint256 tokenID) external onlyOwner {
+        msg.sender.transferToken(_amount, tokenID);
+    }
+
+    function withdrawTRC20(address trc, uint256 _amount) external onlyOwner {
+        TRC20(trc).transfer(msg.sender, _amount);
+    }
+
+    // 转账, 转账资金到指定地址, 由Game合约调用, 当玩家胜利后, Game合约调用此方法发起转账
+    // 权限: gamer
+    // 参数: to 提现地址
+    //      _amount 转账金额
+    function transfer(address  to, uint256 _amount) external onlyGamer{
+        // 转账金额不可超过一定比率
+        require(_amount * leverRadio < address(this).balance);
+        to.transfer(_amount);
+    }
+
+    // 转账, 转账trc10到指定地址, 由Game合约调用, 当玩家胜利后, Game合约调用此方法发起转账
+    // 权限: gamer
+    // 参数: tokenID token的ID
+    //      to 提现地址
+    //      _amount 转账金额
+    function transferToken(address  to, uint256 _amount, uint256 tokenID) external onlyGamer{
+        to.transferToken(_amount, tokenID);
+    }
+
+    function transferTRC20(address to, uint256 _amount, address trc20) external onlyGamer{
+        TRC20(trc20).transfer(to, _amount);
+    }
+
+    function transferTBT(address to,uint256 _amount) external onlyGamer{
+        tbt.transferFrom(msg.sender, to, _amount);
+    }
+
+    // 充值, 向合约转账即表示向合约充值. 玩家失败后, 或者想增加资金数, 由此向合约充值.
+    // 权限: 无
+    function()payable external{}
+}
---
+++ Game.sol
@@ -0,0 +1,225 @@
+pragma solidity >=0.4.0;
+contract CoinPool{
+    function transfer(address to, uint256 _amount) external;
+    function transferTBT(address to,uint256 _amount)external;
+    function ()payable external;
+}
+
+contract Game{
+    address public owner;
+    CoinPool public  _CoinPool;
+    uint256 public tokenIdRTRX;
+    struct BetStruct {
+        address player; // 下注者
+        uint256 blockNumber; // 下注区块号
+        uint256 betType; // 0-9: bet0~bet9 10:small 11:big 12:even 13:odd
+        uint256 tokenValue; // 下注金额
+        uint256 betValue; // 下注金额
+    }
+    event betLog(address player, uint256 betType, uint256 betValue, uint256 tokenValue);
+    event openLog(address player, uint256 betType, uint256 betValue, uint256 tokenValue, bool win);
+    uint256 public nextOpen;
+    BetStruct[] public BetRecord;
+
+    modifier onlyOwner(){require(msg.sender==owner);_;}
+    constructor(address pool, uint256 tokenID)public{
+        owner = msg.sender;
+        _CoinPool = CoinPool(pool);
+        nextOpen = 0;
+        tokenIdRTRX = tokenID;
+    }
+    function switchCoinPool(address pool) external onlyOwner{
+        _CoinPool = CoinPool(pool);
+    }
+    function switchTokenID(uint256 tokenID) external onlyOwner{
+        tokenIdRTRX = tokenID;
+    }
+    function bet(uint256 betType) external payable{
+        if (msg.tokenid == tokenIdRTRX){
+            require(msg.tokenvalue >= 20e6 && msg.tokenvalue < address(_CoinPool).balance/10);
+            _CoinPool.transferTBT(ibet.player, msg.tokenvalue*1e9);
+        }else{
+            require(msg.value >= 20e6 && msg.value < address(_CoinPool).balance/10);
+            _CoinPool.transferTBT(ibet.player, msg.value*1e9);
+        }
+
+        uint256 last = BetRecord.length;
+        BetRecord.length++;
+        BetStruct storage ibet = BetRecord[last];
+        ibet.blockNumber = block.number;
+        ibet.betType = betType;
+        ibet.betValue = msg.value;
+        ibet.player = msg.sender;
+        ibet.tokenValue = msg.tokenvalue;
+        emit betLog(ibet.player, ibet.betType, ibet.betValue, ibet.tokenValue);
+    }
+
+    // 返回: (输赢,注数)
+    function isWin(BetStruct storage ibet) private view returns (bool, uint256) {
+        uint256 bhash = uint256(blockhash(ibet.blockNumber));
+        while ((bhash & 0xf) > 10) {
+            bhash >>= 4;
+        }
+        uint256 betType = ibet.betType;
+        if (((betType>>(bhash&0xf))&0xff)==0)
+            return (false, 0);
+        uint256 n = 0;
+        for(uint256 i = 0; i < 10; i++){
+            if (betType&0xff > 0)
+                n++;
+            betType>>=8;  
+        }
+        return (n > 0, n);
+    }
+
+    function openall() external{
+        open(BetRecord.length);
+    }
+
+    function dealTRX(BetStruct storage ibet, bool win, uint256 n) internal {
+        if (!win){ // loose, transfer TRX to coinpool
+            address(_CoinPool).transfer(ibet.betValue);
+            return;
+        }
+        uint256 total = ibet.betValue*970/100/n;
+        if (total > ibet.betValue){
+            ibet.player.transfer(ibet.betValue);
+            _CoinPool.transfer(ibet.player, total - ibet.betValue);
+        }else{
+            ibet.player.transfer(total);
+            address(_CoinPool).transfer(ibet.betValue-total);
+        }
+    }
+
+    function dealRTRX(BetStruct storage ibet, bool win, uint256 n) internal {
+        if (!win){ // loose, transfer RTRX to coinpool
+            address(_CoinPool).transferToken(ibet.tokenValue, tokenIdRTRX);
+            return;
+        }
+        uint256 total = ibet.tokenValue*970/100/n;
+        if (total > ibet.tokenValue){
+            ibet.player.transferToken(ibet.tokenValue, tokenIdRTRX);
+            _CoinPool.transfer(ibet.player, total - ibet.tokenValue);
+        }else{
+            ibet.player.transferToken(ibet.tokenValue, total);
+            address(_CoinPool).transferToken(total-ibet.tokenValue, tokenIdRTRX);
+        }
+    }
+
+    function open(uint256 num) public{
+        require(BetRecord.length > 0 && BetRecord[0].blockNumber < block.number);
+        uint256 end = num+nextOpen;
+        if (end > BetRecord.length)
+            end = BetRecord.length;
+        uint256 i = 0;
+        for(i = nextOpen; i < end; i++){
+            BetStruct storage ibet = BetRecord[i];
+            if (ibet.blockNumber >= block.number)
+                break;
+
+            (bool win, uint256 n) = isWin(ibet);
+            if(ibet.betValue > 0)
+                dealTRX(ibet, win, n);
+            else
+                dealRTRX(ibet, win, n);
+            emit openLog(ibet.player, ibet.betType, ibet.betValue, ibet.tokenValue, win);
+        }
+        if (i == BetRecord.length){
+            nextOpen = 0;
+            delete BetRecord;
+        }else{
+            nextOpen = i;
+        }
+    }
+
+    function nwin() public view returns(bool, uint256, bytes32, uint256, bytes32) {
+        BetStruct storage ibet = BetRecord[0];
+        uint256 bhash = uint256(blockhash(ibet.blockNumber));
+        while ((bhash & 0xf) > 10) {
+            bhash >>= 4;
+        }
+        uint256 betType = ibet.betType;
+        if (((betType>>(bhash&0xf))&0xff)==0)
+            return (false, 10000, blockhash(ibet.blockNumber), bhash&0xf,blockhash(ibet.blockNumber));
+        uint256 n = 0;
+        for(uint256 i = 0; i < 10; i++){
+            if (betType&0xff > 0)
+                n++;
+            betType>>=8;  
+        }
+        return (n>0, n, blockhash(ibet.blockNumber), bhash&0xf,blockhash(ibet.blockNumber));
+    }
+/*
+    function win() public view returns(bool, uint256) {
+        BetStruct storage ibet = BetRecord[0];
+        uint256 bhash = uint256(blockhash(ibet.blockNumber));
+        while ((bhash & 0xf) > 10) {
+            bhash >>= 4;
+        }
+        uint256 betType = ibet.betType;
+        if (((betType>>(bhash&0xf))&0xff)==0)
+            return (false, 0);
+        uint256 n = 0;
+        for(uint256 i = 0; i < 10; i++){
+            if (betType&0xff > 0)
+                n++;
+            betType>>=8;  
+        }
+        return (n > 0, n);
+    }
+*/
+
+    function open() public view returns(string, uint256, bool, uint256){
+        BetStruct storage ibet = BetRecord[0];
+        (bool win, uint256 n) = isWin(ibet);
+        if(win){
+            if(ibet.betValue > 0){
+                return ("win trx", ibet.betValue*970/100/n, win, n);
+                //ibet.player.transfer(ibet.betValue);
+                //_CoinPool.transfer(ibet.player, ibet.betValue*970/100/n);
+            }else{
+                return ("win trc10", ibet.tokenValue*970/100/n, win, n);
+                //ibet.player.transferToken(ibet.tokenValue, tokenIdRTRX);
+                //_CoinPool.transferToken(ibet.player, ibet.tokenValue*970/100/n, tokenIdRTRX);
+            }
+            //emit openLog(ibet.player, ibet.betType, ibet.betValue, ibet.tokenValue, true);
+        }else{
+            if(ibet.betValue > 0){
+                return ("lose trx", ibet.betValue, win, n);
+                //address(_CoinPool).transfer(ibet.betValue);
+            }else{
+                return ("lose trc10", ibet.tokenValue, win, n);
+                //address(_CoinPool).transferToken(ibet.tokenValue, tokenIdRTRX);
+            }
+            //emit openLog(ibet.player, ibet.betType, ibet.betValue, ibet.tokenValue, false);
+        }
+    }
+
+    function withdraw() external onlyOwner {
+        if (BetRecord.length == 0){
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+
+    function withdrawToken(uint256 tokenID) external onlyOwner {
+        if (BetRecord.length == 0){
+            msg.sender.transferToken(address(this).tokenBalance(tokenID), tokenID);
+        }
+    }
+
+
+    function gbetNum() external view returns(uint256){
+        return BetRecord.length;
+    }
+
+    function gblockhash(uint256 number) public view returns (bytes32, bytes1, uint256){
+        bytes32 h = blockhash(number);
+        return (h, h[31], uint256(h)&0xff);
+    }
+
+    function curblock() public view returns (bytes32, bytes1, uint256){
+        return gblockhash(block.number-1);
+    }
+
+    function ()payable external{}
+}
